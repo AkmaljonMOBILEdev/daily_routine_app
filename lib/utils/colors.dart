f9e532186b92f3254e353d52889973a8a8bafb73
Akmaljon <akmaljondeveloper@gmail.com>
2023-06-23 12:05:27 +0500

    architecture is available without some details

---
+++ lib/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color cFFFFFF=Color(0xFFFFFFFF);
+  static const Color c000000=Color(0xFF000000);
+  static const Color c1C1243=Color(0xFF1C1243);
+  static const Color cEFF1F3=Color(0xFFEFF1F3);
+  static const Color cA29EB6=Color(0xFFA29EB6);
+  static const Color cFFE7CC=Color(0xFFFFE7CC);
+  static const Color c47C272=Color(0xFF47C272);
+
+
+}
