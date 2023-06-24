a76679f6b96bfbc243eb2377868f5dfa0f20fb68
HP <saidrasulovabdulatif5745@gmail.com>
2023-06-24 10:08:43 +0500

    splash screen and welcome screen added

--- lib/ui/onboarding_screens/splash_screen/splash_screen.dart
+++ lib/ui/onboarding_screens/splash_screen/splash_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import '../welcome_screen/welcome_screen.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    _init();
+    super.initState();
+  }
+
+  Future<void> _init() async {
+    await Future.delayed(const Duration(seconds: 3));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return WelcomeScreen();
+      }),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: FlutterLogo(
+          size: 200,
+        ),
+      ),
+    );
   }
 }
