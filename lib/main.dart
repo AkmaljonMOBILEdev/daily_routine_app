f79f7f37a0029df6dfd9a0d9a6caab7fff0030c1
bexruz101 <bexruzyusupov2006@gmail.com>
2023-07-01 15:20:43 +0500

    added app repository and models

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:daily_routine_app/local/storage.dart';
 import 'package:daily_routine_app/utils/app_routes.dart';
 import 'package:daily_routine_app/utils/colors.dart';
 import 'package:daily_routine_app/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'data/local/storage.dart';
 
 Future<void> main()async{
   WidgetsFlutterBinding.ensureInitialized();
